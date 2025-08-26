ad78d325f77151dcc751c2edc689dca2e1efbcdc
Dakugem <Dakugem@mail.ru>
2025-08-26 10:04:08 +0300

    Немного переписал main.cpp, добавил пустые файлы под сервер и клиент

--- Server_Client/src/main.cpp
+++ Server_Client/src/main.cpp
@@ -3,84 +3,101 @@
 
 #include <csignal>
 #include <stdlib.h>
-
+#include <stdexcept>
 #include <atomic>
 #include <string>
 #include <iostream>
 
 std::atomic<bool> STOP{false};
 
-void sigint_handler(int signal_number) {
+void sigint_handler(int signal_number)
+{
   std::cout << "Shutdown started..." << std::endl;
   STOP.load(true);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
   signal(SIGINT, sigint_handler);
-  //server_address Socket
+  // server_address Socket
   bool mode = false; // True - server, false - client
-  //For client only
+  // For client only
   size_t connections = 0;
   size_t seed = 0;
-  
+
   for (int i = 0; i < argc; ++i)
   {
     if (!strcmp(argv[i], "--addr") && argc > i + 1)
     {
-            i++;
-            if (!server_address.set_addr_from_str(argv[i]))
-            {
-              std::cerr << "Invalid server address" << std::endl;
-              return -1;
-            }
-        }
-      
-        if (!strcmp(argv[i], "--mode") && argc > i + 1)
-        {
-            i++;
-
-            if (!strcmp(argv[i], "server") && argc > i + 1){
-              mode = true;
-            } else if (!strcmp(argv[i], "client") && argc > i + 1){
-              mode = false;
-            } else{
-              std::cerr << "Invalid operating mode" << std::endl; 
-              return -1;
-            }
-        }
-
-        if (!strcmp(argv[i], "--connections") && argc > i + 1)
-        {
-            i++;
-
-            //Могут быть ошибки с аргументом argv[i]
-            connections = std::stoull(argv[i]);
-        }
-
-      if (!strcmp(argv[i], "--seed") && argc > i + 1)
-        {
-            i++;
-
-            //Могут быть ошибки с аргументом argv[i]
-            seed = std::stoull(argv[i]);
-        }
+      i++;
+      if (!server_address.set_addr_from_str(argv[i]))
+      {
+        std::cerr << "Invalid server address" << std::endl;
+        return -1;
+      }
     }
 
-  if(mode){
-    Server server{server_address};
+    if (!strcmp(argv[i], "--mode") && argc > i + 1)
+    {
+      i++;
 
-    std::cout << "Server started" << std::endl;
+      if (!strcmp(argv[i], "server") && argc > i + 1)
+      {
+        mode = true;
+      }
+      else if (!strcmp(argv[i], "client") && argc > i + 1)
+      {
+        mode = false;
+      }
+      else
+      {
+        std::cerr << "Invalid operating mode" << std::endl;
+        return -1;
+      }
+    }
 
-    server.run();
-  } else {
-    Client client{server_address, connections, seed};
+    if (!strcmp(argv[i], "--connections") && argc > i + 1)
+    {
+      i++;
 
-    std::cout << "Client started" << std::endl;
+      // Могут быть ошибки с аргументом argv[i]
+      connections = std::stoull(argv[i]);
+    }
 
-    client.run();
+    if (!strcmp(argv[i], "--seed") && argc > i + 1)
+    {
+      i++;
+
+      // Могут быть ошибки с аргументом argv[i]
+      seed = std::stoull(argv[i]);
+    }
   }
 
+  std::unique_ptr<Network_Object> object = nullptr;
+  try
+  {
+    if (mode)
+    {
+      object = std::make_unique<Server>(server_address);
+
+      std::cout << "Server started" << std::endl;
+    }
+    else
+    {
+      object = std::make_unique<Client>(server_address, connections, seed);
+
+      std::cout << "Client started" << std::endl;
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error occured while creating Network_Object: " << e.what << std::endl;
+    return -1;
+  }
+
+  object->run(STOP);
+
   std::cout << "Shutdown complete" << std::endl;
-  
+
   return 0;
 }
